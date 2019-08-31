5c08443424ae6d26ed917ee67f02a14868c2501a
blinkfox <chenjiayin1990@163.com>
2019-09-01 01:07:44 +0800

    修改了 v1.0.1 版本的示例代码,添加了 web 的支持

---
+++ src/main/java/com/blinkfox/fenix/example/controller/IndexController.java
@@ -0,0 +1,53 @@
+package com.blinkfox.fenix.example.controller;
+
+import com.blinkfox.fenix.example.entity.Blog;
+import com.blinkfox.fenix.example.repository.BlogRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * IndexController.
+ *
+ * @author blinkfox on 2019-08-31.
+ */
+@RestController
+@RequestMapping
+public class IndexController {
+
+    @Resource
+    private BlogRepository blogRepository;
+
+    /**
+     * say Hello.
+     *
+     * @return hello
+     */
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+    /**
+     * 查询所有博客信息.
+     *
+     * @return hello
+     */
+    @GetMapping("/blogs")
+    public ResponseEntity<List<Blog>> findBlogs() {
+        // 构造查询的相关参数.
+        List<String> ids = Arrays.asList("1", "2", "3", "4", "5", "6");
+        Blog blog = new Blog().setAuthor("ZhangSan").setUpdateTime(new Date());
+
+        // 查询并返回结果集.
+        List<Blog> blogs = blogRepository.queryBlogsByTemplate(ids, blog);
+        return ResponseEntity.ok(blogs);
+    }
+
+}
